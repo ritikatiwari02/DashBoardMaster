1499958d559abac5d3d97dd2fd78e1ca3c16d2b1
ritika <rt2102000@gmail.com>
2023-08-23 19:16:13 +0530

    Built the cards of home page and sidebar

---
+++ src/components/Charts/PieChart.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const PieChart = () => {
+  return (
+    <div>
+        PieChart
+      
+    </div>
+  )
+}
+
+export default PieChart
---
+++ src/components/Charts/SparkLine.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const SparkLine = () => {
+  return (
+    <div>
+        SparkLine
+      
+    </div>
+  )
+}
+
+export default SparkLine
